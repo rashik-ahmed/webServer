c09958c7fae7a72dcef7c70fc7cc878b2e180ea1
rashik-ahmed <ra3197@nyu.edu>
2024-09-25 22:37:12 -0400

    edit

--- webServer.py
+++ webServer.py
@@ -5,54 +5,79 @@ import sys
 
 
 def webServer(port=13331):
+    # Create a TCP socket using IPv4 (AF_INET) and TCP (SOCK_STREAM)
     serverSocket = socket(AF_INET, SOCK_STREAM)
   
-    # Prepare a server socket
+    # Bind the server socket to the given port on all available network interfaces
     serverSocket.bind(("", port))
-    serverSocket.listen(1)  # Listening for connections, queue size of 1
+    # Start listening for incoming connections, with a queue size of 1 (one connection at a time)
+    serverSocket.listen(1)
   
     while True:
-        # Establish the connection
+        # Display message indicating the server is ready to accept connections
         print('Ready to serve...')
-        connectionSocket, addr = serverSocket.accept()  # Accept connection from client
-    
+        # Accept a connection from a client. This blocks until a connection is received
+        connectionSocket, addr = serverSocket.accept()  # 'addr' holds the client's address
         try:
-            # Receive the request from the client
-            message = connectionSocket.recv(1024).decode()  # Get the client's message and decode it
-            filename = message.split()[1]  # Extract the file requested by the client
-            
-            # Open and read the file requested
-            f = open(filename[1:], 'rb')  # Open the file and read it as bytes
+            # Receive the request sent by the client and decode it to string format
+            try:
+                # Try to receive the data and decode it
+                message = connectionSocket.recv(1024)
+                if message:
+                    message = message.decode()
+                else:
+                    message = ""
+            except UnicodeDecodeError:
+                # Handle decoding errors gracefully
+                message = ""
+            # Split the HTTP request message and extract the filename from the second element of the request line
+            try:
+              request_parts = message.split()
+              filename = request_parts[1] if len(request_parts) > 1 else "/"
+            except IndexError:
+              filename = "/"
+            # Open the requested file from the disk, ignoring the first '/' in the file path, in binary read mode ('rb')
+            f = open(filename[1:], 'rb')
+            # Read the entire file content
             outputdata = f.read()
+            # Close the file after reading
             f.close()
             
-            # Send response header for a valid request
+            # Construct the HTTP response header for a successful file retrieval (200 OK)
             response_header = "HTTP/1.1 200 OK\r\n"
-            response_header += "Content-Type: text/html; charset=UTF-8\r\n"
-            response_header += "Server: Simple-Python-WebServer\r\n"
-            response_header += "Connection: close\r\n"  # Add Connection header
-            response_header += "\r\n"  # End the headers with a blank line
+            response_header += "Content-Type: text/html; charset=UTF-8\r\n"  # Specify content type and character encoding
+            response_header += "Server: Simple-Python-WebServer\r\n"  # Indicate the server's name
+            response_header += "Connection: close\r\n"  # Close the connection after sending the response
+            response_header += "\r\n"  # Blank line to separate headers from body
 
-            # Combine header and body to send in one transmission
+            # Combine the response header and the file content (body) into one message
             response = response_header.encode() + outputdata
-            connectionSocket.send(response)  # Send everything at once
+            # Send the response back to the client
+            connectionSocket.send(response)
 
-            # Close the connection to the client
+            # Close the connection to the client after sending the response
             connectionSocket.close()
     
         except IOError:
-            # Send response for a file not found (404 error)
+            # Handle cases where the requested file is not found by sending a 404 Not Found error
+            
+            # Construct the HTTP response header for a 404 error
             response_header = "HTTP/1.1 404 Not Found\r\n"
-            response_header += "Content-Type: text/html\r\n"
-            response_header += "Connection: close\r\n"
-            response_header += "\r\n"
+            response_header += "Content-Type: text/html\r\n"  # Indicate that the response is HTML
+            response_header += "Connection: close\r\n"  # Close the connection after the response
+            response_header += "\r\n"  # End of the headers
+            
+            # Construct a simple HTML body to display a '404 Not Found' message to the user
             response_body = "<html><body><h1>404 Not Found</h1></body></html>"
+            # Combine the header and body into one message
             response = response_header.encode() + response_body.encode()
 
-            connectionSocket.send(response)  # Send the 404 response in one transmission
+            # Send the 404 error response to the client
+            connectionSocket.send(response)
 
-            # Close the connection to the client
+            # Close the connection to the client after sending the error response
             connectionSocket.close()
 
+# If this script is being run directly (not imported), start the web server on the given port
 if __name__ == "__main__":
-    webServer(13331)
+    webServer(13331)  # Start the server on port 13331
