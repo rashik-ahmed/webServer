8ab2b7a0572c0aefb97a9c092df45304840c1f03
rashik-ahmed <ra3197@nyu.edu>
2024-09-25 22:07:40 -0400

    hw

--- webServer.py
+++ webServer.py
@@ -23,19 +23,19 @@ def webServer(port=13331):
             
             # Open and read the file requested
             f = open(filename[1:], 'rb')  # Open the file and read it as bytes
+            outputdata = f.read()
+            f.close()
             
             # Send response header for a valid request
             response_header = "HTTP/1.1 200 OK\r\n"
             response_header += "Content-Type: text/html; charset=UTF-8\r\n"
             response_header += "Server: Simple-Python-WebServer\r\n"
+            response_header += "Connection: close\r\n"  # Add Connection header
             response_header += "\r\n"  # End the headers with a blank line
 
-            connectionSocket.send(response_header.encode())  # Send the headers
-
-            # Send the content of the requested file to the client
-            outputdata = f.read()
-            connectionSocket.send(outputdata)  # Send file content
-            f.close()
+            # Combine header and body to send in one transmission
+            response = response_header.encode() + outputdata
+            connectionSocket.send(response)  # Send everything at once
 
             # Close the connection to the client
             connectionSocket.close()
@@ -44,10 +44,12 @@ def webServer(port=13331):
             # Send response for a file not found (404 error)
             response_header = "HTTP/1.1 404 Not Found\r\n"
             response_header += "Content-Type: text/html\r\n"
+            response_header += "Connection: close\r\n"
             response_header += "\r\n"
             response_body = "<html><body><h1>404 Not Found</h1></body></html>"
-            connectionSocket.send(response_header.encode())  # Send the headers
-            connectionSocket.send(response_body.encode())  # Send the 404 HTML response
+            response = response_header.encode() + response_body.encode()
+
+            connectionSocket.send(response)  # Send the 404 response in one transmission
 
             # Close the connection to the client
             connectionSocket.close()
